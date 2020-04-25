f37cb800c6b557f138cc016514f8b8f75a7453bb
ChenYiMing <abc102601@gmail.com>
2020-04-25 15:55:50 +0800

    package finish

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 module.exports = {
     entry: './index.js',
-    output:{
-        filename:'index.bundl.js',
-        path: path.resolve(__dirname,'./'),
-    }    
+    output: {
+        filename: 'index.bundle.js',
+        path: path.resolve(__dirname, './'),
+    }
 };
