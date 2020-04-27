3a5f24b6f78ca2bb14a678c7fc1a62bdd748b26e
ChenYiMing <abc102601@gmail.com>
2020-04-27 19:32:10 +0800

    svsva

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
+var webpack = require('webpack');
 const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
     }
 };
