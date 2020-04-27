ea08947b674f95ce44039820b218753787f5e0f9
ChenYiMing <abc102601@gmail.com>
2020-04-27 18:36:03 +0800

    restart

--- README.md
+++
@@ -1,2 +0,0 @@
-# forum
-my studying web script for the forum including html,js,php,css,bootstrap,vue,laravel or any package
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
-        filename: 'index.bundle.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, './'),
     }
 };
